59acbdef2b7bad3a254787f4bd552e871757d01b
robhino <itzepquejguidman@cticoban.edu.gt>
2023-06-23 22:47:09 -0600

    ecuacion de sustituciones resolvido para el su uso

--- calculadoraMateAlgebra/formSustitucion.Designer.cs
+++ calculadoraMateAlgebra/formSustitucion.Designer.cs
@@ -47,6 +47,12 @@ namespace calculadoraMateAlgebra
             this.label7 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.btnLimpiar = new FontAwesome.Sharp.IconButton();
+            this.lblProcesoX = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblResulX = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -82,7 +88,7 @@ namespace calculadoraMateAlgebra
             // 
             // txtc1
             // 
-            this.txtc1.Location = new System.Drawing.Point(252, 64);
+            this.txtc1.Location = new System.Drawing.Point(256, 63);
             this.txtc1.Name = "txtc1";
             this.txtc1.Size = new System.Drawing.Size(31, 20);
             this.txtc1.TabIndex = 4;
@@ -96,7 +102,7 @@ namespace calculadoraMateAlgebra
             // 
             // txtc2
             // 
-            this.txtc2.Location = new System.Drawing.Point(252, 115);
+            this.txtc2.Location = new System.Drawing.Point(256, 114);
             this.txtc2.Name = "txtc2";
             this.txtc2.Size = new System.Drawing.Size(31, 20);
             this.txtc2.TabIndex = 6;
@@ -118,36 +124,40 @@ namespace calculadoraMateAlgebra
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(233, 118);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(232, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.Size = new System.Drawing.Size(18, 20);
             this.label2.TabIndex = 9;
             this.label2.Text = "=";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(233, 71);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.Size = new System.Drawing.Size(18, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "=";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(170, 71);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(165, 66);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.Size = new System.Drawing.Size(18, 20);
             this.label4.TabIndex = 11;
             this.label4.Text = "+";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(170, 119);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(165, 113);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.Size = new System.Drawing.Size(18, 20);
             this.label5.TabIndex = 12;
             this.label5.Text = "+";
             // 
@@ -173,38 +183,42 @@ namespace calculadoraMateAlgebra
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(191, 197);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(205, 412);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.Size = new System.Drawing.Size(186, 31);
             this.label6.TabIndex = 14;
             this.label6.Text = "solucion de x: ";
+            this.label6.Visible = false;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(191, 233);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(205, 468);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.Size = new System.Drawing.Size(187, 31);
             this.label8.TabIndex = 16;
             this.label8.Text = "solucion de y: ";
+            this.label8.Visible = false;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(350, 197);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(425, 412);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(0, 31);
             this.label7.TabIndex = 17;
-            this.label7.Text = "label7";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(350, 233);
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(425, 468);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.Size = new System.Drawing.Size(0, 31);
             this.label9.TabIndex = 18;
-            this.label9.Text = "label9";
             // 
             // btnLimpiar
             // 
@@ -225,11 +239,73 @@ namespace calculadoraMateAlgebra
             this.btnLimpiar.UseVisualStyleBackColor = false;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
+            // lblProcesoX
+            // 
+            this.lblProcesoX.AutoSize = true;
+            this.lblProcesoX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProcesoX.Location = new System.Drawing.Point(206, 186);
+            this.lblProcesoX.Name = "lblProcesoX";
+            this.lblProcesoX.Size = new System.Drawing.Size(0, 25);
+            this.lblProcesoX.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(115, 155);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Procesos de X";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(115, 263);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(71, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Proceso de Y";
+            this.label12.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(206, 298);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 25);
+            this.label13.TabIndex = 24;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(209, 336);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 25);
+            this.label14.TabIndex = 25;
+            // 
+            // lblResulX
+            // 
+            this.lblResulX.AutoSize = true;
+            this.lblResulX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResulX.Location = new System.Drawing.Point(206, 221);
+            this.lblResulX.Name = "lblResulX";
+            this.lblResulX.Size = new System.Drawing.Size(0, 25);
+            this.lblResulX.TabIndex = 21;
+            // 
             // formSustitucion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 521);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lblResulX);
+            this.Controls.Add(this.lblProcesoX);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label7);
@@ -251,6 +327,7 @@ namespace calculadoraMateAlgebra
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formSustitucion";
             this.Text = "v";
+            this.Load += new System.EventHandler(this.formSustitucion_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,5 +353,11 @@ namespace calculadoraMateAlgebra
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label9;
         private FontAwesome.Sharp.IconButton btnLimpiar;
+        private System.Windows.Forms.Label lblProcesoX;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblResulX;
     }
 }
--- calculadoraMateAlgebra/formSustitucion.cs
+++ calculadoraMateAlgebra/formSustitucion.cs
@@ -26,27 +26,51 @@ namespace calculadoraMateAlgebra
 
         private void btnResolver_Click(object sender, EventArgs e)
         {
-            double a1 = Convert.ToDouble(txta1.Text);
-            double b1 = Convert.ToDouble(txtb1.Text);
-            double c1 = Convert.ToDouble(txtc1.Text);
-            double a2 = Convert.ToDouble(txta2.Text);
-            double b2 = Convert.ToDouble(txtb2.Text);
-            double c2 = Convert.ToDouble(txtc2.Text);
+            if(txta1.Text == "")
+            {
+                MessageBox.Show("no se puede ");
+            }
+            else
+            {
+                label6.Visible = true;
+                label8.Visible = true;
+                label11.Visible = true;
+                label12.Visible = true;
 
-            // Resolución de la primera ecuación para x
-            double x = (c1 - b1 * (c2 / b2)) / (a1 - b1 * (a2 / b2));
+                double a1 = Convert.ToDouble(txta1.Text);
+                double b1 = Convert.ToDouble(txtb1.Text);
+                double c1 = Convert.ToDouble(txtc1.Text);
+                double a2 = Convert.ToDouble(txta2.Text);
+                double b2 = Convert.ToDouble(txtb2.Text);
+                double c2 = Convert.ToDouble(txtc2.Text);
 
-            // Sustitución del valor de x en la segunda ecuación
-            double y = (c2 - a2 * x) / b2;
+                // Resolución de la primera ecuación para x
+                double x = (c1 - b1 * (c2 / b2)) / (a1 - b1 * (a2 / b2));
 
-            label7.Text = x.ToString();
-            label9.Text = y.ToString();
+                // Sustitución del valor de x en la segunda ecuación
+                double y = (c2 - a2 * x) / b2;
+                lblProcesoX.Text = " x = (" + c1 + " - " + b1 + " * (" + c2 + " / " + b2 + ")) / (" + a1 + " - " + b1 + " * (" + a2 + " / " + b2 + "))";
+                lblResulX.Text = x.ToString();
+                label13.Text = "y = (" + c2 + " - " + a2 + " * " + x + ") / " + b2 + "";
+
+                label7.Text = x.ToString();
+                label9.Text = y.ToString();
+            }
+           
+
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             txta1.Clear(); txtb1.Clear(); txtc1.Clear();
             txta2.Clear(); txtb2.Clear(); txtc2.Clear();
+            
+            
+        }
+
+        private void formSustitucion_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
