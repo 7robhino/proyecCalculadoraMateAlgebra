c192ee25564309b66500b76318bdd4348d5f4f17
robhino <itzepquejguidman@cticoban.edu.gt>
2023-06-26 23:19:22 -0600

    metodo de igulacion terminada

--- calculadoraMateAlgebra/formIgualacion.Designer.cs
+++ calculadoraMateAlgebra/formIgualacion.Designer.cs
@@ -30,6 +30,28 @@ namespace calculadoraMateAlgebra
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.btnLimpiar = new FontAwesome.Sharp.IconButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMostrarSoluY = new System.Windows.Forms.Label();
+            this.lblMostrarSoluc = new System.Windows.Forms.Label();
+            this.btnResolver = new FontAwesome.Sharp.IconButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txta2 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txtc2 = new System.Windows.Forms.TextBox();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txtc1 = new System.Windows.Forms.TextBox();
+            this.txta1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblTituloX = new System.Windows.Forms.Label();
+            this.lblTituloY = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -46,21 +68,287 @@ namespace calculadoraMateAlgebra
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.IconChar = FontAwesome.Sharp.IconChar.Bath;
+            this.btnLimpiar.IconColor = System.Drawing.Color.Black;
+            this.btnLimpiar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnLimpiar.IconSize = 35;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(488, 94);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(88, 36);
+            this.btnLimpiar.TabIndex = 41;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(465, 479);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 31);
+            this.label9.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(465, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 31);
+            this.label7.TabIndex = 39;
+            // 
+            // lblMostrarSoluY
+            // 
+            this.lblMostrarSoluY.AutoSize = true;
+            this.lblMostrarSoluY.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrarSoluY.Location = new System.Drawing.Point(245, 479);
+            this.lblMostrarSoluY.Name = "lblMostrarSoluY";
+            this.lblMostrarSoluY.Size = new System.Drawing.Size(187, 31);
+            this.lblMostrarSoluY.TabIndex = 38;
+            this.lblMostrarSoluY.Text = "solucion de y: ";
+            this.lblMostrarSoluY.Visible = false;
+            // 
+            // lblMostrarSoluc
+            // 
+            this.lblMostrarSoluc.AutoSize = true;
+            this.lblMostrarSoluc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrarSoluc.Location = new System.Drawing.Point(245, 423);
+            this.lblMostrarSoluc.Name = "lblMostrarSoluc";
+            this.lblMostrarSoluc.Size = new System.Drawing.Size(186, 31);
+            this.lblMostrarSoluc.TabIndex = 37;
+            this.lblMostrarSoluc.Text = "solucion de x: ";
+            this.lblMostrarSoluc.Visible = false;
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnResolver.FlatAppearance.BorderSize = 0;
+            this.btnResolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResolver.IconChar = FontAwesome.Sharp.IconChar.Equals;
+            this.btnResolver.IconColor = System.Drawing.Color.Black;
+            this.btnResolver.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnResolver.IconSize = 35;
+            this.btnResolver.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnResolver.Location = new System.Drawing.Point(366, 94);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(88, 36);
+            this.btnResolver.TabIndex = 36;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnResolver.UseVisualStyleBackColor = false;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(194, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(194, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(261, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "=";
+            // 
+            // txta2
+            // 
+            this.txta2.Location = new System.Drawing.Point(147, 122);
+            this.txta2.Name = "txta2";
+            this.txta2.Size = new System.Drawing.Size(38, 20);
+            this.txta2.TabIndex = 31;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Location = new System.Drawing.Point(218, 122);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(37, 20);
+            this.txtb2.TabIndex = 30;
+            // 
+            // txtc2
+            // 
+            this.txtc2.Location = new System.Drawing.Point(285, 124);
+            this.txtc2.Name = "txtc2";
+            this.txtc2.Size = new System.Drawing.Size(31, 20);
+            this.txtc2.TabIndex = 29;
+            // 
+            // txtb1
+            // 
+            this.txtb1.Location = new System.Drawing.Point(218, 74);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(37, 20);
+            this.txtb1.TabIndex = 28;
+            // 
+            // txtc1
+            // 
+            this.txtc1.Location = new System.Drawing.Point(285, 73);
+            this.txtc1.Name = "txtc1";
+            this.txtc1.Size = new System.Drawing.Size(31, 20);
+            this.txtc1.TabIndex = 27;
+            // 
+            // txta1
+            // 
+            this.txta1.Location = new System.Drawing.Point(147, 74);
+            this.txta1.Name = "txta1";
+            this.txta1.Size = new System.Drawing.Size(38, 20);
+            this.txta1.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(258, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 39);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Igualacion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(132, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(217, 22);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Pasos De Procedimientos";
+            this.label6.Visible = false;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(235, 249);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(0, 13);
+            this.lblX.TabIndex = 50;
+            this.lblX.Visible = false;
+            this.lblX.Click += new System.EventHandler(this.lblX_Click);
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(229, 308);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(0, 13);
+            this.lblY.TabIndex = 51;
+            this.lblY.Visible = false;
+            // 
+            // lblTituloX
+            // 
+            this.lblTituloX.AutoSize = true;
+            this.lblTituloX.Location = new System.Drawing.Point(126, 213);
+            this.lblTituloX.Name = "lblTituloX";
+            this.lblTituloX.Size = new System.Drawing.Size(59, 13);
+            this.lblTituloX.TabIndex = 52;
+            this.lblTituloX.Text = "Resolver X";
+            this.lblTituloX.Visible = false;
+            // 
+            // lblTituloY
+            // 
+            this.lblTituloY.AutoSize = true;
+            this.lblTituloY.Location = new System.Drawing.Point(126, 282);
+            this.lblTituloY.Name = "lblTituloY";
+            this.lblTituloY.Size = new System.Drawing.Size(59, 13);
+            this.lblTituloY.TabIndex = 53;
+            this.lblTituloY.Text = "Resolver Y";
+            this.lblTituloY.Visible = false;
+            // 
             // formIgualacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 521);
+            this.Controls.Add(this.lblTituloY);
+            this.Controls.Add(this.lblTituloX);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblMostrarSoluY);
+            this.Controls.Add(this.lblMostrarSoluc);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txta2);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txtc2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.txtc1);
+            this.Controls.Add(this.txta1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formIgualacion";
             this.Text = "formIgualacion";
+            this.Load += new System.EventHandler(this.formIgualacion_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private FontAwesome.Sharp.IconButton btnLimpiar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMostrarSoluY;
+        private System.Windows.Forms.Label lblMostrarSoluc;
+        private FontAwesome.Sharp.IconButton btnResolver;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txta2;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txtc2;
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.TextBox txtc1;
+        private System.Windows.Forms.TextBox txta1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblTituloX;
+        private System.Windows.Forms.Label lblTituloY;
     }
 }
--- calculadoraMateAlgebra/formIgualacion.cs
+++ calculadoraMateAlgebra/formIgualacion.cs
@@ -19,7 +19,59 @@ namespace calculadoraMateAlgebra
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Close();
+            this.Close();
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            if (txta1.Text == "")
+            {
+                MessageBox.Show("no se puede ");
+            }
+            else
+            { 
+                //mostrar Resultados
+                lblMostrarSoluc.Visible = true;
+                lblMostrarSoluY.Visible = true;
+
+                float a = Convert.ToSingle(txta1.Text);
+                float b = Convert.ToSingle(txtb1.Text);
+                float c = Convert.ToSingle(txtc1.Text);
+                float d = Convert.ToSingle(txta2.Text);
+                float e2 = Convert.ToSingle(txtb2.Text);
+                float f = Convert.ToSingle(txtc2.Text);
+
+                float x = (c * e2 - b * f) / (a * e2 - b * d);
+                float y = (a * f - c * d) / (a * e2 - b * d);
+
+                lblTituloX.Visible = true;
+                lblX.Text = " x = (" + c + " * " + e2 + " - " + b + " * " + f + ") / (" + a + " * " + e2 + " - " + b + " * " + d + "";
+                lblX.Visible = true;
+
+                lblTituloY.Visible = true;
+                lblY.Text = "y = ("+a+" * "+f+" - "+c+" * "+d+") / ("+a+" * "+e2+" - "+b+" * "+d+")";
+                lblY.Visible = true;
+
+                label7.Text = x.ToString();
+                label9.Text = y.ToString();
+            }
+
+        }
+
+        private void formIgualacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblX_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txta1.Clear(); txtb1.Clear(); txtc1.Clear();
+            txta2.Clear(); txtb2.Clear(); txtc2.Clear();
         }
     }
 }
