794cac918e47fb5731ca01f50cb294166a34a8e2
robhino <itzepquejguidman@cticoban.edu.gt>
2023-07-01 20:06:14 -0600

    metodo grafico nuevo

--- calculadoraMateAlgebra/formGraficaAlg.Designer.cs
+++ calculadoraMateAlgebra/formGraficaAlg.Designer.cs
@@ -30,6 +30,25 @@ namespace calculadoraMateAlgebra
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new FontAwesome.Sharp.IconButton();
+            this.btnResolver = new FontAwesome.Sharp.IconButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txta2 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txtc2 = new System.Windows.Forms.TextBox();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txtc1 = new System.Windows.Forms.TextBox();
+            this.txta1 = new System.Windows.Forms.TextBox();
+            this.lblSolucX = new System.Windows.Forms.Label();
+            this.lblSolucY = new System.Windows.Forms.Label();
+            this.lblProceso = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblProcess = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -46,21 +65,280 @@ namespace calculadoraMateAlgebra
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 43);
+            this.label1.TabIndex = 83;
+            this.label1.Text = "Forma Grafica";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.IconChar = FontAwesome.Sharp.IconChar.Bath;
+            this.btnLimpiar.IconColor = System.Drawing.Color.Black;
+            this.btnLimpiar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnLimpiar.IconSize = 35;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(519, 93);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(88, 36);
+            this.btnLimpiar.TabIndex = 82;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnResolver.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnResolver.FlatAppearance.BorderSize = 0;
+            this.btnResolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResolver.IconChar = FontAwesome.Sharp.IconChar.Equals;
+            this.btnResolver.IconColor = System.Drawing.Color.Black;
+            this.btnResolver.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnResolver.IconSize = 35;
+            this.btnResolver.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnResolver.Location = new System.Drawing.Point(397, 93);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(88, 36);
+            this.btnResolver.TabIndex = 81;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnResolver.UseVisualStyleBackColor = false;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(225, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 80;
+            this.label5.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(225, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 79;
+            this.label4.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(292, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 78;
+            this.label3.Text = "=";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(292, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 77;
+            this.label2.Text = "=";
+            // 
+            // txta2
+            // 
+            this.txta2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txta2.Location = new System.Drawing.Point(178, 121);
+            this.txta2.Name = "txta2";
+            this.txta2.Size = new System.Drawing.Size(38, 20);
+            this.txta2.TabIndex = 76;
+            this.txta2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txta2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txta2_KeyPress);
+            // 
+            // txtb2
+            // 
+            this.txtb2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtb2.Location = new System.Drawing.Point(249, 121);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(37, 20);
+            this.txtb2.TabIndex = 75;
+            this.txtb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtb2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtb2_KeyPress);
+            // 
+            // txtc2
+            // 
+            this.txtc2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtc2.Location = new System.Drawing.Point(316, 123);
+            this.txtc2.Name = "txtc2";
+            this.txtc2.Size = new System.Drawing.Size(31, 20);
+            this.txtc2.TabIndex = 74;
+            this.txtc2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtc2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtc2_KeyDown);
+            // 
+            // txtb1
+            // 
+            this.txtb1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtb1.Location = new System.Drawing.Point(249, 73);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(37, 20);
+            this.txtb1.TabIndex = 73;
+            this.txtb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtb1_KeyPress);
+            // 
+            // txtc1
+            // 
+            this.txtc1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtc1.Location = new System.Drawing.Point(316, 72);
+            this.txtc1.Name = "txtc1";
+            this.txtc1.Size = new System.Drawing.Size(31, 20);
+            this.txtc1.TabIndex = 72;
+            this.txtc1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtc1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtc1_KeyPress);
+            // 
+            // txta1
+            // 
+            this.txta1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txta1.Location = new System.Drawing.Point(178, 73);
+            this.txta1.Name = "txta1";
+            this.txta1.Size = new System.Drawing.Size(38, 20);
+            this.txta1.TabIndex = 71;
+            this.txta1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txta1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txta1_KeyPress);
+            // 
+            // lblSolucX
+            // 
+            this.lblSolucX.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSolucX.AutoSize = true;
+            this.lblSolucX.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolucX.Location = new System.Drawing.Point(275, 434);
+            this.lblSolucX.Name = "lblSolucX";
+            this.lblSolucX.Size = new System.Drawing.Size(0, 22);
+            this.lblSolucX.TabIndex = 84;
+            // 
+            // lblSolucY
+            // 
+            this.lblSolucY.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSolucY.AutoSize = true;
+            this.lblSolucY.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolucY.Location = new System.Drawing.Point(275, 474);
+            this.lblSolucY.Name = "lblSolucY";
+            this.lblSolucY.Size = new System.Drawing.Size(0, 22);
+            this.lblSolucY.TabIndex = 85;
+            // 
+            // lblProceso
+            // 
+            this.lblProceso.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblProceso.AutoSize = true;
+            this.lblProceso.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProceso.Location = new System.Drawing.Point(86, 173);
+            this.lblProceso.Name = "lblProceso";
+            this.lblProceso.Size = new System.Drawing.Size(142, 24);
+            this.lblProceso.TabIndex = 86;
+            this.lblProceso.Text = "Procedimiento";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(113, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 22);
+            this.label6.TabIndex = 87;
+            this.label6.Text = "Solucion de X";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(113, 474);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 22);
+            this.label7.TabIndex = 88;
+            this.label7.Text = "Solucion Y";
+            this.label7.Visible = false;
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProcess.Location = new System.Drawing.Point(104, 206);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(0, 20);
+            this.lblProcess.TabIndex = 89;
+            // 
             // formGraficaAlg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 560);
+            this.Controls.Add(this.lblProcess);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblProceso);
+            this.Controls.Add(this.lblSolucY);
+            this.Controls.Add(this.lblSolucX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txta2);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txtc2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.txtc1);
+            this.Controls.Add(this.txta1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formGraficaAlg";
             this.Text = "formGraficaAlg";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconButton btnLimpiar;
+        private FontAwesome.Sharp.IconButton btnResolver;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txta2;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txtc2;
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.TextBox txtc1;
+        private System.Windows.Forms.TextBox txta1;
+        private System.Windows.Forms.Label lblSolucX;
+        private System.Windows.Forms.Label lblSolucY;
+        private System.Windows.Forms.Label lblProceso;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblProcess;
     }
 }
--- calculadoraMateAlgebra/formGraficaAlg.cs
+++ calculadoraMateAlgebra/formGraficaAlg.cs
@@ -21,5 +21,97 @@ namespace calculadoraMateAlgebra
         {
             this.Close();
         }
+
+        private void txta1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                txtb1.Focus();
+            }
+        }
+
+        private void txtb1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                txtc1.Focus();
+            }
+        }
+
+        private void txtc1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                txta2.Focus();
+            }
+        }
+
+        private void txta2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                txtb2.Focus();
+            }
+        }
+
+        private void txtb2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                txtc2.Focus();
+            }
+        }
+
+        private void txtc2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                btnResolver.PerformClick();
+            }
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            if (txta1.Text == " ")
+            {
+                MessageBox.Show("NO SE PUEDE ^0^");
+            }
+            else
+            {
+                float a1 = Convert.ToSingle(txta1.Text);
+                float b1 = Convert.ToSingle(txtb1.Text);
+                float a2 = Convert.ToSingle(txta2.Text); //txtb2.Text
+                float b2 = Convert.ToSingle(txtb2.Text);
+                //terminos independientes
+                float c1 = Convert.ToSingle(txtc1.Text);
+                float c2 = Convert.ToSingle(txtc2.Text);
+
+                float y1 = (c1 * a2 - a1 * c2) / (a2 * b1 - a1 * b2);
+                float x1 = (c1 - b1 * y1) / a1;
+
+                string procesus = $"Resolver y\n";
+                procesus += $"y1 = ("+c1+" * "+a2+" - "+a1+" * "+c2+")\n / ("+a2+" * "+b1+" - "+a1+" * "+b2+") \n";
+                procesus += $"\nResolver x\n";
+                procesus += "x1 = (" + c1 + " - " + b1 + " * " + y1 + ") / " + a1 + "\n";
+
+                lblProcess.Text = procesus;
+                label6.Visible = true;
+                lblSolucX.Text = x1.ToString();
+                label7.Visible = true;
+                lblSolucY.Text = y1.ToString();
+             }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txta1.Clear(); txtb1.Clear(); txtc1.Clear();
+            txta2.Clear(); txtb2.Clear(); txtc2.Clear();
+        }
     }
 }
